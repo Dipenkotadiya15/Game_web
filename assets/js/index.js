cb490b1e9b8aa34dce0b5379a17cd9978be52c93
Dipenkotadiya15 <dipen.adsum@gmail.com>
2022-12-27 18:02:46 +0530

    first

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $('.dropdownli').click (function() {
+//         $('.dropdown-item').slideToggle('slow');
+//     });
+// });
+
+
+$(document).ready(function() {
+    $('.toggle').click(function() {
+        $('#nav1').slideToggle('slow')
+    })
+})
